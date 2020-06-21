2a017f2b7dab3ee7b1c6f7ac955d1d0d7bd2e07f
nguyensonhai <nguyensonhai1009@gmail.com>
2020-06-22 00:51:22 +0700

    update

--- routes/AppStack.js
+++ routes/AppStack.js
@@ -1,17 +1,17 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
-import Login from '../screens/login';
-import Home from '../screens/home';
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import Login from "../screens/login";
+import Home from "../screens/home";
 
 const screens = {
-    Login: {
-        screen: Login,
-        navigationOptions: { headerShown:false }
-    },
-    Home: {
-        screen: Home,
-        navigationOptions: { headerShown:false }
-    },
+  Login: {
+    screen: Login,
+    navigationOptions: { headerShown: false },
+  },
+  Home: {
+    screen: Home,
+    navigationOptions: { headerShown: false },
+  },
 };
 
 const AppStack = createStackNavigator(screens);
--- screens/home.js
+++ screens/home.js
@@ -1,138 +1,183 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, Image } from 'react-native';
-import * as Font from 'expo-font';
-import Loader from '../shared/Loader';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import * as Font from "expo-font";
+import Loader from "../shared/Loader";
 export default class Home extends Component {
-
   state = {
-    username: this.props.navigation.getParam('username', ''),
+    username: this.props.navigation.getParam("username", ""),
     data: [],
     devices: [],
     assetsLoaded: false,
     device1On: false,
     device2On: false,
-    deviceOn: require('../assets/images/onSwitch.png'),
-    deviceOff: require('../assets/images/offSwitch.png'),
-  }
+    deviceOn: require("../assets/images/onSwitch.png"),
+    deviceOff: require("../assets/images/offSwitch.png"),
+  };
 
   async componentDidMount() {
-    await fetch('https://eztreecare.herokuapp.com/NhanTuServer.php?username=' + this.state.username)
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({ data: responseJson })
+    await fetch(
+      "https://eztreecare.herokuapp.com/NhanTuServer.php?username=" +
+        this.state.username
+    )
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({ data: responseJson });
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
 
-    await fetch('https://eztreecare.herokuapp.com/ShowDevicesStatus.php?username=' + this.state.username)
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({ devices: responseJson })
-        if(responseJson[0].device_status == 'on') {
+    await fetch(
+      "https://eztreecare.herokuapp.com/ShowDevicesStatus.php?username=" +
+        this.state.username
+    )
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({ devices: responseJson });
+        if (responseJson[0].device_status == "on") {
           this.setState({ device1On: true });
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
-      await fetch('https://eztreecare.herokuapp.com/ShowDevicesStatus.php?username=' + this.state.username)
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({ devices: responseJson })
-        if(responseJson[1].device_status == 'on') {
+    await fetch(
+      "https://eztreecare.herokuapp.com/ShowDevicesStatus.php?username=" +
+        this.state.username
+    )
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({ devices: responseJson });
+        if (responseJson[1].device_status == "on") {
           this.setState({ device2On: true });
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
 
     this.setState({ assetsLoaded: true });
-    this.interval = setInterval(
-      () => {
-        fetch('https://eztreecare.herokuapp.com/NhanTuServer.php?username=' + this.state.username)
-          .then(response => response.json())
-          .then(responseJson => {
-            this.setState({ data: responseJson })
-          })
-          .catch(error => {
-            console.error(error);
-          });
-        fetch('https://eztreecare.herokuapp.com/ShowDevicesStatus.php?username=' + this.state.username)
-          .then(response => response.json())
-          .then(responseJson => {
-            this.setState({ devices: responseJson })
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      },
-      500
-    );
+    this.interval = setInterval(() => {
+      fetch(
+        "https://eztreecare.herokuapp.com/NhanTuServer.php?username=" +
+          this.state.username
+      )
+        .then((response) => response.json())
+        .then((responseJson) => {
+          this.setState({ data: responseJson });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      fetch(
+        "https://eztreecare.herokuapp.com/ShowDevicesStatus.php?username=" +
+          this.state.username
+      )
+        .then((response) => response.json())
+        .then((responseJson) => {
+          this.setState({ devices: responseJson });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }, 500);
   }
 
-  toggleDevice = async number => {
+  toggleDevice = async (number) => {
     var request = new XMLHttpRequest();
     switch (number) {
       case 1:
         if (!this.state.device1On) {
-          request.open('GET', 'http://eztreecare.herokuapp.com/UpdateDevicesStatus.php?device_name=device1&device_status=on&username=' + this.state.username);
+          request.open(
+            "GET",
+            "http://eztreecare.herokuapp.com/UpdateDevicesStatus.php?device_name=device1&device_status=on&username=" +
+              this.state.username
+          );
           request.send();
         } else {
-          request.open('GET', 'http://eztreecare.herokuapp.com/UpdateDevicesStatus.php?device_name=device1&device_status=off&username=' + this.state.username);
+          request.open(
+            "GET",
+            "http://eztreecare.herokuapp.com/UpdateDevicesStatus.php?device_name=device1&device_status=off&username=" +
+              this.state.username
+          );
           request.send();
         }
         this.setState({ device1On: !this.state.device1On });
         break;
       case 2:
         if (!this.state.device2On) {
-          request.open('GET', 'http://eztreecare.herokuapp.com/UpdateDevicesStatus.php?device_name=device2&device_status=on&username=' + this.state.username);
+          request.open(
+            "GET",
+            "http://eztreecare.herokuapp.com/UpdateDevicesStatus.php?device_name=device2&device_status=on&username=" +
+              this.state.username
+          );
           request.send();
         } else {
-          request.open('GET', 'http://eztreecare.herokuapp.com/UpdateDevicesStatus.php?device_name=device2&device_status=off&username=' + this.state.username);
+          request.open(
+            "GET",
+            "http://eztreecare.herokuapp.com/UpdateDevicesStatus.php?device_name=device2&device_status=off&username=" +
+              this.state.username
+          );
           request.send();
         }
-        this.setState({ device2On: !this.state.device2On })
+        this.setState({ device2On: !this.state.device2On });
         break;
       default:
         break;
     }
-  }
+  };
 
   render() {
     const { assetsLoaded } = this.state;
 
     if (assetsLoaded) {
       return (
-        <ImageBackground source={require('../assets/images/greenBackground.jpg')} style={styles.container}>
+        <ImageBackground
+          source={require("../assets/images/greenBackground.jpg")}
+          style={styles.container}
+        >
           <Text style={styles.text}>Độ ẩm: {this.state.data[0].doam} </Text>
 
           <View style={styles.containerButton}>
-
             <View style={styles.butt}>
-              <Text style={styles.textDevice} >Device 1</Text>
+              <Text style={styles.textDevice}>Device 1</Text>
               <TouchableOpacity onPress={() => this.toggleDevice(1)}>
-                <Image style={styles.imageButt} source={this.state.device1On ? this.state.deviceOn : this.state.deviceOff} />
+                <Image
+                  style={styles.imageButt}
+                  source={
+                    this.state.device1On
+                      ? this.state.deviceOn
+                      : this.state.deviceOff
+                  }
+                />
               </TouchableOpacity>
             </View>
 
             <View style={styles.butt}>
-              <Text style={styles.textDevice} >Device 2</Text>
+              <Text style={styles.textDevice}>Device 2</Text>
               <TouchableOpacity onPress={() => this.toggleDevice(2)}>
-                <Image style={styles.imageButt} source={this.state.device2On ? this.state.deviceOn : this.state.deviceOff} />
+                <Image
+                  style={styles.imageButt}
+                  source={
+                    this.state.device2On
+                      ? this.state.deviceOn
+                      : this.state.deviceOff
+                  }
+                />
               </TouchableOpacity>
             </View>
-
           </View>
         </ImageBackground>
       );
-    }
-    else {
-      return (
-        <Loader
-          loading={this.state.loading} />
-      );
+    } else {
+      return <Loader loading={this.state.loading} />;
     }
   }
 }
@@ -140,27 +185,27 @@ export default class Home extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     padding: 20,
     marginBottom: 250,
-    fontFamily: 'quicksand-bold',
+    fontFamily: "quicksand-bold",
     fontSize: 40,
-    color: 'black',
-    backgroundColor: 'white',
-    borderRadius: 200
+    color: "black",
+    backgroundColor: "white",
+    borderRadius: 200,
   },
   butt: {
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
   textDevice: {
-    fontFamily: 'quicksand-bold',
+    fontFamily: "quicksand-bold",
     fontSize: 22,
-    color: 'white',
+    color: "white",
   },
   imageButt: {
     marginTop: 20,
@@ -168,8 +213,8 @@ const styles = StyleSheet.create({
     height: 55,
   },
   containerButton: {
-    flexDirection: 'row',
-    width: '80%',
-    justifyContent: 'space-around',
-  }
+    flexDirection: "row",
+    width: "80%",
+    justifyContent: "space-around",
+  },
 });
